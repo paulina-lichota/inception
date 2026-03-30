6c20a1f0e243d36fecd87ed055dd5d8030bb3838
Paulina Lichota <paulinalichota1@gmail.com>
2026-03-31 01:09:40 +0200

    Dockerfile wordpress

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Inizializza il database solo se non esiste già
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+    mysqld_safe --skip-networking &
+    sleep 3
+
+    mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$(cat /run/secrets/db_root_password)');
+FLUSH PRIVILEGES;
+EOF
+
+    kill $(cat /var/run/mysqld/mysqld.pid)
+    sleep 1
+
+fi
+
+exec "$@"
